6a96ae5035a7d32fc027d9d47b52f300088d0aa0
NinjaAung <ninja.aung02@gmail.com>
2023-01-30 13:33:59 -0800

    Created Footer and Header
    Added bootstrap and removed auth on home page

--- stock-market/vue/src/App.vue
+++ stock-market/vue/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
+    <Header class="navbar navbar-expand-ig bd-navbar sticky-top" />
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
+    <footer />
   </div>
 </template>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script>
+import Header from './components/Header.vue';
+export default {
+  components: {
+    Header,
+  }
+
+}
+</script>
+
+<style>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100vw;
+  background-color: #FFB703;
+  padding: 30px
+}
+</style>
