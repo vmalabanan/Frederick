2729a2515ed6774a88bb5afc0cc7305599d128bd
Nyein Chan Aung <ninja.aung02@gmail.com>
2023-01-31 18:20:21 +0000

    Vue games list

--- stock-market/vue/src/views/GameList.vue
+++ stock-market/vue/src/views/GameList.vue
@@ -1,20 +1,16 @@
 <template>
     <div id="GameList">
         <div class="card w-50">
-            <div class="card-header" style="background-color: #FFB703; border-radius:25px; height: 60px"></div>
-            <div class="card-body">
-                <div id="dummy_data">
-                    <div id="name">MANGA FTW</div>
-                    <div id="type">Normal</div>
-                    <div id="Length">1 Week</div>
-                    <button id="join">Join</button>
-                </div>
-                <div id="dummy_data">
-                    <div id="name">MANGA FTW</div>
-                    <div id="type">Normal</div>
-                    <div id="Length">1 Week</div>
-                    <button id="join">Join</button>
-                </div>
+            <div class="card-header" style="background-color: #FFB703; border-radius:25px; height: 60px">
+                <span>Game Name</span>
+                <span>Type</span>
+                <span>Length</span>
+                <i class="fa-sharp fa-solid fa-xmark-large"></i>
+            </div>
+            <div id="games" class="card-body">
+                <Game name="MANGA FTW" type="Normal" duration="1 Week" />
+                <Game name="MANGA FTW" type="Normal" duration="1 Week" />
+                <Game name="MANGA FTW" type="Normal" duration="1 Week" />
             </div>
             <div class="card-footer" style="background-color: #FFB703; border-radius:25px; height: 60px">
             </div>
@@ -22,12 +18,24 @@
     </div>
 </template>
 <script>
+import Game from '../components/Game.vue';
 export default {
     name: "GameList",
+    components: {
+        Game,
+    }
 }
 </script>
 
 <style>
+div#GameList {
+    display: flex;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+}
+
+
 div#GameList>div {
     background-color: #8ECAE6;
     border-radius: 25px;
@@ -35,32 +43,9 @@ div#GameList>div {
     border: none;
 }
 
-div#dummy_data {
+div.card-header {
     display: flex;
-    background-color: #023047;
-    border-radius: 15px;
-    height: 60px;
-    color: white;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
-    padding: 20px;
-    margin: 10px;
 }
-
-button#join {
-    background-color: #FB8500;
-    border-radius: 15px;
-    border: none;
-    width: 100px;
-    height: 50px;
-}
-
-
-
-
-/* 
-div#GameList>div>div:first-child :last-child {
-    background-color: #FB8500;
-    border-radius: 25px;
-} */
-</style>
+</style>5
