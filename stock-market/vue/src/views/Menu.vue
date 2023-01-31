170c2654a9a23798e362c1315dee649156dad3a4
Vanessa Malabanan <vanessamalabanan@yahoo.com>
2023-01-30 17:16:03 -0800

    style menu page

--- stock-market/vue/src/views/Menu.vue
+++ stock-market/vue/src/views/Menu.vue
@@ -1,17 +1,38 @@
 <template>
-  <div>
-      <button>Host Game</button>
-      <button>Join Game</button>
-      <button>My Games</button>
+  <div class="options-container">
+    <div class="options">
+      <router-link :to="{ name: 'create' }">
+        <button class="btn btn-lg btn-primary btn-block">Host Game</button>
+      </router-link>
+      <router-link :to="{ name: 'create' }">
+        <button class="btn btn-lg btn-primary btn-block">Join Game</button>
+      </router-link>
+      <router-link :to="{ name: 'portfolio' }">
+        <button class="btn btn-lg btn-primary btn-block">My Games</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "menu",
-}
+  name: "menu"
+};
 </script>
 
 <style>
 
+.options-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+
+}
+
+.options {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  /* justify-content: center; */
+}
 </style>
