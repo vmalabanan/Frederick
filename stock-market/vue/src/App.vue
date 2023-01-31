dfd9f70fb29930560f4aad429d3ccc471fc7aad5
NinjaAung <ninja.aung02@gmail.com>
2023-01-31 10:40:56 -0800

    removed header

--- stock-market/vue/src/App.vue
+++ stock-market/vue/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <Header class="navbar navbar-expand-ig bd-navbar sticky-top" />
+    <header class="navbar navbar-expand-ig bd-navbar sticky-top">
+      <img src="./img/frederick_logo.svg" alt="Frederick the goldfish!" />
+      <span>Welcome</span>
+      <img style="transform: scaleX(-1);" src="./img/frederick_logo.svg" alt="Frederick the goldfish!" />
+    </header>
+
+
     <!-- <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
@@ -13,11 +19,16 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <script>
-import Header from './components/Header.vue';
 export default {
-  components: {
-    Header,
-  }
-
+  name: 'App'
 }
 </script>
+
+<style >
+header {
+  background-color: #023047;
+  color: #FB8500;
+  font-size: 60px;
+  font-weight: bold;
+}
+</style>
