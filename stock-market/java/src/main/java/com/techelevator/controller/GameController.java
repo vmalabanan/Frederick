af1b7b79abddb379956d3a6ba2a463e2010f08b5
Jonathan Thompson <jonathanwthompson98@gmail.com>
2023-01-30 16:56:34 -0800

    Added foundation for game controller/jdbc. Added TODOs for jdbc

--- stock-market/java/src/main/java/com/techelevator/controller/GameController.java
+++ stock-market/java/src/main/java/com/techelevator/controller/GameController.java
@@ -1,5 +1,27 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.GameDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/games")
 public class GameController
 {
+    private GameDao gameDao;
+
+    public GameController(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
+
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @RequestMapping(value = "/created", method = RequestMethod.POST)
+//    public void createGame(@Valid @RequestBody Game gameDto) {
+//
+//    }
+
+
 }
