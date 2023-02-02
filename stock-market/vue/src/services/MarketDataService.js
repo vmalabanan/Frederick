2961fde385ec2b7004a3f04526e92adecd4db126
Jonathan Thompson <jonathanwthompson98@gmail.com>
2023-02-02 14:50:07 -0800

    Added extra endpoints to market service

--- stock-market/vue/src/services/MarketDataService.js
+++ stock-market/vue/src/services/MarketDataService.js
@@ -8,8 +8,38 @@ const API_KEY = "apikey=4694ab156015fcae69356511ac9a8a36"
 export default {
 
 	//gets minimum last 6 days of data
-	getHistoricalDataBySymbol(symbol) {
+	getHistoricalDailyDataBySymbol(symbol) {
 		let url = "historical-price-full/" + symbol + "?from=";
+		let today = new Date();
+		let week = new Date();
+		week.setDate(week.getDate() - 8)
+		let todayString = today.toISOString()
+		let delimiter = todayString.indexOf("T")
+		const endDate = todayString.substring(0, delimiter)
+		let fromString = week.toISOString()
+		delimiter = fromString.indexOf("T")
+		const fromDate = fromString.substring(0, delimiter)
+		url += fromDate + "&to=" + endDate + "&" + API_KEY
+		return http.get(url)
+	},
+
+    getHistoricalHourDataBySymbol(symbol) {
+		let url = "historical-chart/1hour/" + symbol + "?from=";
+		let today = new Date();
+		let week = new Date();
+		week.setDate(week.getDate() - 8)
+		let todayString = today.toISOString()
+		let delimiter = todayString.indexOf("T")
+		const endDate = todayString.substring(0, delimiter)
+		let fromString = week.toISOString()
+		delimiter = fromString.indexOf("T")
+		const fromDate = fromString.substring(0, delimiter)
+		url += fromDate + "&to=" + endDate + "&" + API_KEY
+		return http.get(url)
+	},
+
+    getHistoricalMinuteDataBySymbol(symbol) {
+		let url = "historical-chart/1min/" + symbol + "?from=";
 		let today = new Date();
 		let week = new Date();
 		week.setDate(week.getDate() - 8)
