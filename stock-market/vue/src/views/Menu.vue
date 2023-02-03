6aba2e2ed6408026ea4cb26a18eab3d9f34a3c55
Vanessa Malabanan <vanessamalabanan@yahoo.com>
2023-02-02 17:22:51 -0800

    change routing on menu page's 'my games' button to go to first game in list of user's accepted games

--- stock-market/vue/src/views/Menu.vue
+++ stock-market/vue/src/views/Menu.vue
@@ -4,25 +4,54 @@
       <button class="btn btn-lg btn-primary" @click="createPage">
         Host Game
       </button>
-      <button class="btn btn-lg btn-warning" @click="gameList">Game Invites</button>
-      <button class="btn btn-lg btn-info" @click="portfolio">My Games</button>
+      <button
+        class="btn btn-lg btn-warning"
+        v-if="$store.state.invitedGames.length > 0"
+        @click="invitationsList"
+      >
+        Game Invites
+      </button>
+      <button
+        class="btn btn-lg btn-info"
+        v-if="$store.state.acceptedGames.length > 0"
+        @click="portfolio"
+      >
+        My Games
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import gamesService from "../services/GamesService.js";
+
 export default {
   name: "menu",
   methods: {
     createPage() {
       this.$router.push({ name: "createGame" });
     },
-    gameList() {
+    invitationsList() {
       this.$router.push({ name: "invitationsList" });
     },
     portfolio() {
-      this.$router.push({ name: "portfolio" }); // add id param here. should default to one of the player's games
+      const firstGame = this.$store.state.acceptedGames[0];
+      this.$router.push({
+        name: "portfolio",
+        params: { id: firstGame.gameId }
+      }); 
     }
+  },
+  created() {
+    // get list of game invitations and add to store
+    gamesService.getInvitedGames().then(resp => {
+      this.$store.commit("SET_INVITED_GAMES", resp.data);
+    });
+
+    // get list of all accepted games and add to store
+    gamesService.getAcceptedGames().then(resp => {
+      this.$store.commit("SET_ACCEPTED_GAMES", resp.data);
+    });
   }
 };
 </script>
