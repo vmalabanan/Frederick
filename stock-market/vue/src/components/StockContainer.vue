65aebd0dc0601a4bf0b17c14b4b49020c1bb1d3b
Jonathan Thompson <jonathanwthompson98@gmail.com>
2023-02-06 10:51:08 -0800

    Added my portfolio header to make screens more distinct

--- stock-market/vue/src/components/StockContainer.vue
+++ stock-market/vue/src/components/StockContainer.vue
@@ -1,8 +1,12 @@
 <template>
-	<div id="stocks">
-		<stock-card v-for="(stock, index) in stocks" :key="index" v-bind="stock"
-			@click.native="handleClick(stock.symbol)" v-model="buySellCard" />
+	<div id="stock-container">
+		<h2 v-show="onPortfolio">My Portfolio</h2>
+		<div id="stocks">
+			<stock-card v-for="(stock, index) in stocks" :key="index" v-bind="stock"
+				@click.native="handleClick(stock.symbol)" v-model="buySellCard" />
+		</div>
 	</div>
+
 </template>
 
 <script>
@@ -10,7 +14,7 @@ import StockCard from './StockCard.vue'
 export default {
 	name: 'StockContainer',
 	components: { StockCard },
-	props: ['stocks', 'value'],
+	props: ['stocks', 'value', 'onPortfolio'],
 	emits: ['cardClick'],
 	data() {
 		return {
@@ -39,12 +43,25 @@ export default {
 	display: flex;
 	flex-wrap: wrap;
 	align-content: flex-start;
+
+
+	border-radius: 20px;
+	margin-bottom: 20%;
+}
+
+#stock-container {
 	background-color: #023047;
 	min-height: 500px;
 	width: 100%;
 	box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
 		0 10px 10px rgba(0, 0, 0, 0.22);
-	border-radius: 20px;
-	margin-bottom: 20%;
+}
+h2 {
+	display: flex;
+	justify-content: center;
+	color: #fb8500;
+	padding-top: 10px;
+	text-decoration: underline;
+	font-weight: bold;
 }
 </style>
