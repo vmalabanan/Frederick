62f914f92dbdcb127eb23a5af378898141942c49
Jonathan Thompson <jonathanwthompson98@gmail.com>
2023-02-06 10:23:02 -0800

    added changes to dynamically change game name at top of header

--- stock-market/vue/src/App.vue
+++ stock-market/vue/src/App.vue
@@ -4,22 +4,26 @@
       <img src="./img/frederick_logo.svg" alt="Frederick the goldfish!" />
       <active-games v-if="this.$route.name == 'portfolio'"></active-games>
       <span>{{ this.header[this.$route.name] }}</span>
-      <img style="transform: scaleX(-1);" src="./img/frederick_logo.svg" alt="Frederick the goldfish!" />
+      <img
+        style="transform: scaleX(-1)"
+        src="./img/frederick_logo.svg"
+        alt="Frederick the goldfish!"
+      />
     </header>
-
 
     <!-- <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div> -->
     <router-view />
-    <footer class="fixed-bottom px-4 py-4" style="background-color: #FFB703;" />
+    <footer class="fixed-bottom px-4 py-4" style="background-color: #ffb703" />
   </div>
 </template>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <script>
+import gamesService from "./services/GamesService"
 import ActiveGames from "./components/ActiveGames.vue"
 
 export default {
@@ -31,8 +35,26 @@ export default {
         createGame: 'Create a Game',
         invitationsList: 'Join a Game',
         menu: 'Game Menu',
-        portfolio: 'Game Name goes here'
-      },
+        portfolio: ''
+      }
+    }
+  },
+  watch: {
+    $route (to, from) {
+      this.getGameName()
+    }
+  },
+  created() {
+    this.getGameName()
+  },
+  computed: {
+    getGameName() {
+        if (this.$route.name == 'portfolio') {
+        const gameId = this.$route.params.id
+        gamesService.getGameById(gameId).then(resp => {
+          this.header.portfolio = resp.data.gameName
+        })
+      }
     }
   },
   components: {ActiveGames}
@@ -42,7 +64,7 @@ export default {
 <style >
 header {
   background-color: #023047;
-  color: #FB8500;
+  color: #fb8500;
   font-size: 60px;
   font-weight: bold;
 }
