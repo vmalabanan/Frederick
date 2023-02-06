c1fe27b4ce75c98ae85e4c5780681c600da1e85f
NinjaAung <ninja.aung02@gmail.com>
2023-02-06 13:55:16 -0800

    Added logger

--- stock-market/java/src/main/java/com/techelevator/controller/ChatController.java
+++ stock-market/java/src/main/java/com/techelevator/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.techelevator.controller;
+
 import com.techelevator.model.Message;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
     @MessageMapping("/game")
     @SendTo("/topic/chat")
-    public Message broadcastMessage(Message message)  {
+    public Message broadcastMessage(Message message) {
         return message;
     }
 }
