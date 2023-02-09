6cd715e6602d766632bbae823bbaa77638f4a7f7
Vanessa Malabanan <vanessamalabanan@yahoo.com>
2023-02-09 02:45:48 -0800

    style games list screen

--- stock-market/vue/src/views/GamesList.vue
+++ stock-market/vue/src/views/GamesList.vue
@@ -1,45 +1,53 @@
 <template>
-  <div id="GameList">
+  <div id="gamelist">
     <hamburger class="hamburger" :routeNames="hamburgerLinks"></hamburger>
-
     <p class="no-invites-msg" v-if="$store.state.acceptedGames.length === 0">
       No games to display
     </p>
-    <div class="card w-50" v-else>
-      <div
-        class="card-header"
-        style="background-color: #ffb703; border-radius: 25px; height: 60px"
-      >
-        <span style="width: 330px">Game Name</span>
-        <span style="width: 170px">Type</span>
-        <span style="width: 200px">Length</span>
-        <span style="width: 100px"></span>
-      </div>
-      <div id="games" class="card-body">
-        <Game
-          v-for="(game, index) in $store.state.acceptedGames"
-          :key="index"
-          :game="game"
+    <div v-else class="gamelist-content">
+      <div class="fish-container">
+        <img
+          class="fish-2"
+          src="../img/goldfish-in-place.gif"
+          alt="goldfish gif"
+        />
+
+        <img
+          class="fish"
+          src="../img/goldfish-in-place.gif"
+          alt="goldfish gif"
+        />
+        <img
+          class="fish-3"
+          src="../img/goldfish-in-place.gif"
+          alt="goldfish gif"
         />
       </div>
-      <div
-        class="card-footer"
-        style="background-color: #ffb703; border-radius: 25px; height: 60px"
-      />
+
+      <div class="all-button-container">
+        <div class="gamelist-container">
+          <button
+            class="btn btn-lg btn-primary"
+            v-for="(game, index) in $store.state.acceptedGames"
+            :key="index"
+            @click="viewGame(game.gameId)"
+          >
+            {{ game.gameName }}
+          </button>
+        </div>
+        <button class="btn btn-lg btn-info" id="back" @click="$router.go(-1)">
+          Back
+        </button>
+      </div>
     </div>
-    <button class="btn btn-lg btn-info" id="back" @click="$router.go(-1)">
-      Back
-    </button>
   </div>
 </template>
 <script>
-import Game from "../components/Game.vue";
 import Hamburger from "../components/Hamburger.vue";
 
 export default {
   name: "GamesList",
   components: {
-    Game,
     Hamburger,
   },
   data() {
@@ -47,34 +55,115 @@ export default {
       hamburgerLinks: ["home"],
     };
   },
+  methods: {
+    viewGame(gameId) {
+      this.$router.push({
+        name: "portfolio",
+        params: { id: gameId },
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
-#GameList {
-	
+#gamelist {
+  background-color: #ffe9e1;
+  height: 100vh;
 }
-div#GameList {
+.no-invites-msg {
+  font-size: 2rem;
+  text-align: center;
+}
+.gamelist-content {
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  margin-left: -10rem;
+}
+.fish-container {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  /* margin-top: 5%; */
+  align-items: flex-end;
+margin-right: 2rem;
 }
 
-div#GameList > div {
-  background-color: #8ecae6;
-  border-radius: 25px;
-  margin: 0 auto;
-  border: none;
+.fish {
+  transform: rotate(7deg);
+  position: relative;
+  animation-name: float;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
 }
 
-div.card-header {
+.fish-2,
+.fish-3 {
+  width: 30%;
+  transform: rotate(5deg);
+  margin-top: 3rem;
+  margin-right: 5rem;
+}
+.gamelist-container {
+  /* background-color: #ffb703; */
+  /* width: 20%; */
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
 }
 
-div#GameList > #back {
-  /* margin-top: 20px; */
+.all-button-container {
+  margin-top: 7rem;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+button {
+  border: none;
+  box-shadow: 0.5rem 0.5rem #ffb703;
+  color: #fff;
+}
+
+.btn-primary {
+  background-color: #477fc9;
+}
+
+.btn-primary:hover {
+  background-color: #1566d1;
+  color: #000;
+}
+
+.btn-info {
+  background-color: #8ecae6;
+}
+
+.btn-info:hover {
+  background-color: #65bce4;
+}
+
+@keyframes float {
+  0% {
+    top: 0px;
+  }
+
+  25% {
+    top: 20px;
+  }
+
+  50% {
+    top: 30px;
+  }
+
+  75% {
+    top: 20px;
+  }
+
+  100% {
+    top: 0px;
+  }
 }
 </style>
