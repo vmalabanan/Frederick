609491317212c5c3ee372ff1a3e3b79dfca4e609
Jonathan Thompson <jonathanwthompson98@gmail.com>
2023-02-10 10:01:38 -0800

    Switched socket port

--- stock-market/vue/src/services/SocketService.js
+++ stock-market/vue/src/services/SocketService.js
@@ -1,8 +1,8 @@
 import SockJS from "sockjs-client";
 import Stomp from "webstomp-client";
 
-//const BASE_SOCKET = "http://34.102.118.144:8080/"
-const BASE_SOCKET = "http://localhost:8080/"
+const BASE_SOCKET = "http://34.102.118.144:8080/"
+// const BASE_SOCKET = "http://localhost:8080/"
 // const SUB_POINT = "topic/"
 // const SEND_POINT = "app/"
 
